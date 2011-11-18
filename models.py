02f65fada6f41f63937276aae7e24be3b752fe58
Grigouze <grigouze@yahoo.fr>
2011-11-18 08:27:18 +0100

    the view and the models

---
+++ models.py
@@ -0,0 +1,90 @@
+import socket
+import urllib2
+import json
+
+class JenkinsCI(object):
+    """
+        The real class for get information in jenkins server
+    """
+
+    def __init__(self, url=None, view=None, timeout=0):
+        if url is None:
+            url = 'http://' + socket.gethostname()
+        if view is None:
+            view = ''
+        else:
+            view = '/view/%s' % view
+
+        self.url = url
+        self.view = view
+        self.timeout = timeout
+
+        return None
+
+    def urlopen(self, url):
+        """
+            Binding to urllib2.urlopen
+        """
+
+        timeout = self.timeout
+
+        return urllib2.urlopen(url, timeout=timeout)
+
+    def get_all_jobs(self):
+        """
+            List all jobs of a view for the jenkins server
+        """
+
+        datas = self.urlopen('%s%s/api/json?tree=jobs[name,color]' % (self.url, \
+                            self.view))
+
+        if not datas:
+            raise 'Error getting build data from Jenkins server at %s' \
+                    % self.url
+
+        ret = []
+
+        jobs = json.loads(datas.read())
+        for job in jobs['jobs']:
+            newjob = {'name': job['name'], 'status': \
+                self.translate_color_to_status(job['color']), 'blame': ''}
+            if newjob['status'][0] in ('failed', 'unstable'):
+                newjob['blame'] = self.get_blame_for(job['name'])
+
+            ret.append(newjob)
+
+        return ret
+
+    @staticmethod
+    def translate_color_to_status(color):
+        """
+            Translate color of jenkins to status for view
+        """
+
+        colors = {'blue': ['successful'], \
+                'blue_anime': ['successful','building'], \
+                'red': ['failed'], \
+                'red_anime': ['failed','building'], \
+                'yellow': ['unstable'], \
+                'yellow_anime': ['unstable', 'building'], \
+                'aborted': ['cancelled'], \
+                'aborted_anime': ['cancelled','building'], \
+                'disabled': ['disabled'], \
+                'default': ['unknown']}
+
+        return colors.get(color, colors.get('default'))
+
+    def get_blame_for(self, jobname):
+        """
+            If you have to blame someone for bad work :)
+        """
+
+        datas = self.urlopen("%s/job/%s/lastBuild/api/json?tree=culprits[fullName]" \
+                                % (self.url, jobname))
+
+        culprits = json.loads(datas.read())
+
+        if not culprits.get('culprits'):
+            return "Unknown"
+
+        return culprits['culprits'][0]['fullName']
---
+++ radiator.py
@@ -0,0 +1,52 @@
+import json
+from flask import Flask, render_template, request
+from models import JenkinsCI
+
+app = Flask(__name__)
+app.config.from_object('settings')
+
+import json
+
+from models import JenkinsCI
+
+@app.route('/')
+def index():
+    view = request.args.get('view', '')
+    return render_template('index.html', view=view)
+
+@app.route('/builds')
+def builds():
+    """
+        Return the json data of jenkins server
+    """
+
+    url = app.config['JENKINS_URL']
+    view = request.args.get('view') or app.config['JENKINS_DEFAULT_URL']
+    timeout = app.config['JENKINS_TIMEOUT']
+
+    jenkins = JenkinsCI(url=url, view=view, timeout=timeout)
+
+    jobs = None
+    result = None
+
+    try:
+        jobs = jenkins.get_all_jobs()
+    except Exception, error:
+        result = {'status': 'error', 'content': str(error.reason)}
+
+    if not result:
+        html = ''
+        for job in jobs:
+            blame = job['blame']
+            if job['status'][0] in ('failed', 'unstable'):
+                blame = "<br /><span class='blame'>%s</span>" % job['blame']
+
+            html += "<li class = 'job " + " ".join(job['status']) + \
+                    "'>%s%s</li>" % (job['name'], blame)
+
+            result = {'status': 'ok', 'content': html}
+
+    return json.dumps(result)
+
+if __name__ == '__main__':
+    app.run(debug=True)
